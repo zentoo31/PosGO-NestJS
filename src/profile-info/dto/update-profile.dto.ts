c8b5a084b7b0ff0f4942a379075327bfe8dc9a99
Diego Pineda <zentoo31@gmail.com>
2025-05-16 21:41:12 -0500

    add validation for update-profile dto

--- src/profile-info/dto/update-profile.dto.ts
+++ src/profile-info/dto/update-profile.dto.ts
@@ -1,7 +1,15 @@
+import { IsString } from "class-validator";
 
 export class UpdateProfileDto {
+    @IsString()
     pic_url: string;
+
+    @IsString()
     name: string;
+
+    @IsString()
     city: string;
+
+    @IsString()
     country: string;
 }
