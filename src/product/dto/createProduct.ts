8adc46a68a8dd93b7d3d331c8b24ce82bdcda0d6
Diego Pineda <zentoo31@gmail.com>
2025-06-10 17:41:19 -0500

    update: add class validator methods

--- src/product/dto/createProduct.ts
+++ src/product/dto/createProduct.ts
@@ -1,9 +1,23 @@
+import { IsString, IsNumber, MaxLength } from "class-validator";
 
 export class CreateProductDto {
+    @MaxLength(100)
+    @IsString()
     name: string;
+
+    @MaxLength(500)
+    @IsString()
     description: string;
+
+    @IsNumber()
     price: number;
+        
+    @IsString()
     category?: string;
+    
+    @IsNumber()
     stock: number;
+        
+    @IsString()
     imageUrl: string;
 }
