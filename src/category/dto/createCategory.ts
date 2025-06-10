60bd52df4c8b2a1a341833a15686d5cfd96f1c7e
Diego Pineda <zentoo31@gmail.com>
2025-06-10 17:53:32 -0500

    feat: add create category dto with validators

---
+++ src/category/dto/createCategory.ts
@@ -0,0 +1,11 @@
+import { IsString, MaxLength } from "class-validator";
+
+export class CreateCategoryDto {
+    @MaxLength(50)
+    @IsString()
+    name: string;
+
+    @MaxLength(200)
+    @IsString()
+    description: string;
+}
