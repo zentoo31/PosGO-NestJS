e919abf1f8d12e3b82f3bcf4ab33e3104df068dc
Diego Pineda <zentoo31@gmail.com>
2025-06-11 12:56:03 -0500

    feat: implement profile info retrieval and update with error handling

--- src/profile-info/profile-info.controller.ts
+++ src/profile-info/profile-info.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, UsePipes, ValidationPipe, Body, Param, HttpException, HttpStatus  } from '@nestjs/common';
+import { ProfileInfoService } from './profile-info.service';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profile-info')
-export class ProfileInfoController {}
+export class ProfileInfoController {
+    constructor(private readonly profileInfoService: ProfileInfoService) {}
+
+    @Get()
+    async getProfileInfo(@Param('id') id: string){
+        try {
+            return await this.profileInfoService.getProfileInfo(id);
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.INTERNAL_SERVER_ERROR,
+                error: `${error.message}`,
+            }, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Post()
+    @UsePipes(new ValidationPipe())
+    async updateProfileInfo(@Body() updateProfileDto: UpdateProfileDto, @Param('id') id: string) {
+        try {
+            return await this.profileInfoService.updateProfileInfo(id, updateProfileDto);
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.BAD_REQUEST,
+                error: `${error.message}`,
+            }, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
