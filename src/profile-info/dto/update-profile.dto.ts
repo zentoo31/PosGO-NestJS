007ab8e9e757c3ab77bdb8a80f87a9909360277f
Diego Pineda <zentoo31@gmail.com>
2025-06-11 12:34:13 -0500

    feat: add optional validation to UpdateProfileDto fields

--- src/profile-info/dto/update-profile.dto.ts
+++ src/profile-info/dto/update-profile.dto.ts
@@ -1,15 +1,19 @@
-import { IsString } from "class-validator";
+import { IsString, IsOptional } from "class-validator";
 
 export class UpdateProfileDto {
     @IsString()
+    @IsOptional()
     pic_url: string;
 
     @IsString()
+    @IsOptional()
     name: string;
 
     @IsString()
+    @IsOptional()
     city: string;
 
     @IsString()
+    @IsOptional()
     country: string;
 }
