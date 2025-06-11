cd4971d242a9e24bcab7c7f73dd9cb73c7b4f230
Diego Pineda <zentoo31@gmail.com>
2025-06-11 09:12:08 -0500

    feat: implement createProduct and getProducts methods in ProductService with error handling

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -9,9 +9,34 @@ export class ProductService {
         private supabase: SupabaseClient
     ){}
     
-    async createProduct(product: CreateProductDto){
+    async createProduct(product: CreateProductDto) {
+        const existingCategory = await this.supabase
+            .schema('public')
+            .from('category')
+            .select('*')
+            .eq('id', product.category)
+            .single();
+        if (!existingCategory.data) throw new Error('Category not found');
+        if (existingCategory.error) throw new Error(existingCategory.error.message);
+
+        const { data, error } = await this.supabase
+            .schema('public')
+            .from('product')
+            .insert(product)
+            .select('*')
+            .single();
         
+        if (error) throw new Error(error.message);
+        return { id: data.id, message: 'Product created successfully' };
     }
 
+    async getProducts(){
+        const {data, error} =  await this.supabase
+            .schema('public')
+            .from('product')
+            .select('*');
+        if (error) throw new Error(error.message);
+        return data;
+    }
 
 }
