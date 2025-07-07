c875b77160a0bdac835ec58f4fb04d12c1980d72
Diego Pineda <zentoo31@gmail.com>
2025-07-06 23:21:13 -0500

    feat: update getHello method to return HTML response for thank you page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response): void {
+    const html = this.appService.getThankYouHTML();
+    res.setHeader('Content-Type', 'text/html');
+    res.send(html);
   }
 }
